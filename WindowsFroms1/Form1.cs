81afbea57d022d1261a7c4cc67e9dd883f7f69d7
shaun51 <shaun.nichols51@gmail.com>
2014-04-25 23:33:41 -0700

    Adding Button Event

---
+++ WindowsFroms1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFroms1
+{
+    public partial class MyDesktopApp : Form
+    {
+        public MyDesktopApp()
+        {
+            InitializeComponent();
+        }
+
+        private void changeText_Click(object sender, EventArgs e)
+        {
+            if (enableCheckbox.Checked == true)
+            {
+                if (labelToChange.Text == "Right")
+                {
+                    labelToChange.Text = "Left";
+                    labelToChange.TextAlign = ContentAlignment.MiddleLeft;
+                }
+                else
+                {
+                    labelToChange.Text = "Right";
+                    labelToChange.TextAlign = ContentAlignment.MiddleRight;
+                }
+
+            }
+            else
+            {
+ 
+            }
+        }
+    }
+}
